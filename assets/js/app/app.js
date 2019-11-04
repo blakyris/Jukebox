13f4136546a6be4e5ba4708e46381ca3ac9425b9
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2019-11-04 18:54:59 +0100

    created basic UI

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Sidebar from './components/Sidebar/Sidebar';
+import LibraryExplorer from './components/LibraryExplorer/LibraryExplorer';
+import Player from './components/Player/Player';
+
 class App extends React.Component {
 
     componentDidMount() {
-        
+
     }
 
     render() {
         return (
-            <div className="container">
-                <h1>Hello Symfony !</h1>
-            </div>  
+            <div className="d-flex flex-column p-0 h-100">
+                <div className="d-flex flex-row vh-100">
+                        <Sidebar />
+                        <LibraryExplorer />
+                </div>
+                <Player />
+            </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />
+    , document.getElementById('root'));
+
