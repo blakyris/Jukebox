d5e489b5df58e5f7e75681f06e5c9822d98d6d26
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2019-11-11 16:35:10 +0100

    Removed Symfony Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
 ];
